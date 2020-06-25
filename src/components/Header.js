521bb5506dfa6fc2247b4fcd3d4630aa6e46d457
Diana kagotho <dianakago2@gmail.com>
2020-06-25 08:24:34 -0400

    modified header.js and header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>You can use the search box to narrow your results.</p>
+        <p>You can use the search box for faster search.</p>
       </div>
     )
   }
